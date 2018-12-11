2f72e5543c430e762576aeaf9b15fac31443348b
fen.wen <fen.wen@56linked.com>
2018-12-11 11:53:30 +0800

    //

--- src/API/index.js
+++ src/API/index.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const getLacation = "http//www.quickxs.com/vcloudwood-gateway/vcloudwood/gateway/query.v";
 let localUrl='https://gwt.56linked.com';//本地调接口
-let devUrl = 'http://192.168.1.181:22226'; //胡欢
 let devURL = 'http://testllservice.56linked.cn';//测试环境
 let prodURL = 'http://www.quickxs.com';//正式环境
 
